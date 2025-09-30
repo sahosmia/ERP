21022743de20e2f75855a2da8a086508150dee08
sahosmia <sahosmia.webdev@gmail.com>
2025-09-30 13:33:00 +0600

    fix few isses

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FabricApiController;
 use App\Http\Controllers\Api\SupplierApiController;
-use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('suppliers', SupplierApiController::class);
-Route::apiResource('fabrics', FabricApiController::class);
+Route::prefix('api/')->name('api.')->group(function () {
+    //
+    Route::apiResource('suppliers', SupplierApiController::class);
+    Route::apiResource('fabrics', FabricApiController::class);
+
+});
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+Route::middleware(['auth', 'verified'])->prefix('admin/')->name('admin.')->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
